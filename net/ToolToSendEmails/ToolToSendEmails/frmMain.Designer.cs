6386f51e5a7c60d1c0a52598621403709908c599
joemismito <joemismito@78ca7313-de5c-acb9-4185-dfd521fc74ab>
2010-04-07 14:36:53 +0000

    ToolToSendEmails

---
+++ net/ToolToSendEmails/ToolToSendEmails/frmMain.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ToolToSendEmails
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btSend = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkTest = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtBody = new Design.Editor();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labInfo = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtMaxAddress = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMaxAddress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFrom.Location = new System.Drawing.Point(62, 19);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(528, 20);
+            this.txtFrom.TabIndex = 1;
+            this.txtFrom.Text = "no-reply@egeneralmedical.com";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSubject.Location = new System.Drawing.Point(62, 45);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(528, 20);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Subject";
+            // 
+            // btSend
+            // 
+            this.btSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSend.Location = new System.Drawing.Point(574, 18);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(75, 23);
+            this.btSend.TabIndex = 6;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMaxAddress);
+            this.groupBox1.Controls.Add(this.chkTest);
+            this.groupBox1.Controls.Add(this.txtFrom);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSubject);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 81);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // chkTest
+            // 
+            this.chkTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkTest.AutoSize = true;
+            this.chkTest.Checked = true;
+            this.chkTest.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkTest.Location = new System.Drawing.Point(596, 18);
+            this.chkTest.Name = "chkTest";
+            this.chkTest.Size = new System.Drawing.Size(47, 17);
+            this.chkTest.TabIndex = 9;
+            this.chkTest.Text = "Test";
+            this.chkTest.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBody);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 81);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(655, 466);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Body";
+            // 
+            // txtBody
+            // 
+            this.txtBody.BodyHtml = null;
+            this.txtBody.BodyText = null;
+            this.txtBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBody.DocumentText = resources.GetString("txtBody.DocumentText");
+            this.txtBody.EditorBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBody.EditorForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBody.FontName = null;
+            this.txtBody.FontSize = Design.FontSize.NA;
+            this.txtBody.Location = new System.Drawing.Point(3, 16);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(649, 447);
+            this.txtBody.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labInfo);
+            this.groupBox3.Controls.Add(this.progressBar1);
+            this.groupBox3.Controls.Add(this.btSend);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox3.Location = new System.Drawing.Point(0, 547);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(655, 53);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // labInfo
+            // 
+            this.labInfo.AutoSize = true;
+            this.labInfo.Location = new System.Drawing.Point(4, 16);
+            this.labInfo.Name = "labInfo";
+            this.labInfo.Size = new System.Drawing.Size(48, 13);
+            this.labInfo.TabIndex = 8;
+            this.labInfo.Text = "Progress";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(559, 10);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // txtMaxAddress
+            // 
+            this.txtMaxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMaxAddress.Increment = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.txtMaxAddress.Location = new System.Drawing.Point(596, 45);
+            this.txtMaxAddress.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.txtMaxAddress.Minimum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.txtMaxAddress.Name = "txtMaxAddress";
+            this.txtMaxAddress.Size = new System.Drawing.Size(53, 20);
+            this.txtMaxAddress.TabIndex = 10;
+            this.txtMaxAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtMaxAddress.Value = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 600);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Tool To Send Emails";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMaxAddress)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private Design.Editor txtBody;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labInfo;
+        private System.Windows.Forms.CheckBox chkTest;
+        private System.Windows.Forms.NumericUpDown txtMaxAddress;
+    }
+}
+
---
+++ net/ToolToSendEmails/ToolToSendEmails/frmMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace ToolToSendEmails
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+                if (!string.IsNullOrEmpty(this.txtFrom.Text))
+                {
+                    this.progressBar1.Minimum = 0;
+                    this.progressBar1.Maximum = 2;
+                    this.progressBar1.Value = 0;
+                    int taskNumber = 0;
+
+                    string from = this.txtFrom.Text;
+                    string subject = this.txtSubject.Text;
+                    string body = this.txtBody.BodyHtml;
+                    string to = string.Empty;
+
+                    MailMessage msg = new MailMessage();
+                    msg.Subject = subject;
+                    msg.Body = body;
+                    msg.From = new MailAddress(from);
+                    msg.IsBodyHtml = true;
+
+                    SmtpClient client = new SmtpClient(Properties.Settings.Default.smtpServer);
+                    client.UseDefaultCredentials = true;
+
+                    this.labInfo.Text = "Loading customers email addresses...";
+                    this.progressBar1.Value = taskNumber++;
+                    smallmvc.customerDAO customerDAO = new ToolToSendEmails.smallmvc.customerDAO();
+                    List<smallmvc.customerVO> listCustomers = customerDAO.GetAll();
+
+                    this.labInfo.Text = "Sending emails...";
+                    this.progressBar1.Value = taskNumber++;
+
+                    System.Text.RegularExpressions.Regex regEx = new System.Text.RegularExpressions.Regex(@"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+     + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+     + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+     + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$");
+
+                    int maxAddress = Convert.ToInt32(txtMaxAddress.Value);
+                    foreach (smallmvc.customerVO customer in listCustomers)
+                    {
+                        try
+                        {
+                            if (this.chkTest.Checked)
+                                to = from;
+                            else
+                                to = customer.Email;
+                            if ((regEx.IsMatch(from)) && (regEx.IsMatch(to)))
+                            {
+                                msg.Bcc.Add(new MailAddress(to));
+
+                                if (msg.Bcc.Count >= maxAddress)
+                                {
+                                    client.Send(msg);
+                                    msg.Bcc.Clear();
+                                }
+                            }
+                            if (this.chkTest.Checked) 
+                                break;
+                        }
+                        catch
+                        {
+                        }
+                    }
+
+                    this.progressBar1.Value = taskNumber++;
+                    MessageBox.Show("Messages sent!");
+                }
+                else
+                    MessageBox.Show("You must enter from address!");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message); ;
+            }
+            finally
+            {
+                this.Cursor = System.Windows.Forms.Cursors.Default;
+            }
+           
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Properties.Settings.Default.fromAddress = this.txtFrom.Text;
+                Properties.Settings.Default.Save();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.txtFrom.Text = Properties.Settings.Default.fromAddress;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
