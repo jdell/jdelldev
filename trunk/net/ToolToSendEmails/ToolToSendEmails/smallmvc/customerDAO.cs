2e6a8525e22bb0517fb0153fb3984b67d59a128e
joemismito <joemismito@78ca7313-de5c-acb9-4185-dfd521fc74ab>
2010-04-07 14:36:53 +0000

    ToolToSendEmails

---
+++ trunk/net/ToolToSendEmails/ToolToSendEmails/smallmvc/customerDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolToSendEmails.smallmvc
+{
+    internal class customerDAO
+    {
+        public List<customerVO> GetAll()
+        {
+            System.Data.Common.DbProviderFactory factory = null;
+            System.Data.Common.DbConnection connection = null;
+            System.Data.Common.DbCommand command = null;
+
+            try
+            {
+                List<customerVO> res = new List<customerVO>();
+
+                System.Configuration.ConnectionStringSettings wholeConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ToolToSendEmails.Properties.Settings.mas90"];
+
+                factory = System.Data.Common.DbProviderFactories.GetFactory(wholeConnectionString.ProviderName);
+                connection = factory.CreateConnection();
+                connection.ConnectionString = wholeConnectionString.ConnectionString;
+                command = connection.CreateCommand();
+
+                connection.Open();
+
+                command.CommandText = "SELECT * FROM OPENQUERY(MAS90, 'SELECT * FROM AR1_CustomerMaster') AR1_CustomerMaster WHERE (EmailAddress <> '')";
+                System.Data.Common.DbDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    customerVO customer = new customerVO();
+
+                    if (!Convert.IsDBNull(reader["CustomerName"])) customer.Name = Convert.ToString(reader["CustomerName"]);
+                    if (!Convert.IsDBNull(reader["EmailAddress"])) customer.Email = Convert.ToString(reader["EmailAddress"]);
+
+                    res.Add(customer);
+                }
+
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if ((connection != null) && (connection.State == System.Data.ConnectionState.Open)) connection.Close();
+            }
+        }
+    }
+}
---
+++ trunk/net/ToolToSendEmails/ToolToSendEmails/smallmvc/customerVO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolToSendEmails.smallmvc
+{
+    internal class customerVO
+    {
+        private string _name = string.Empty;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value.Trim(); }
+        }
+        private string _email = string.Empty;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value.Trim(); }
+        }
+    }
+}
